fd2127c6bca3388c7beafb83dfa3d76953f11146
WilliamSturgiss <william.sturgiss@cordatahealth.com>
2017-02-11 00:29:50 -0500

    removed sass and got bootstrap working along with the horse

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    sassOptions: {
-      includePaths: [
-        'bower_components/bootstrap-sass/assets/stylesheets'
-      ]
-    }
     // Add options here
   });
 
@@ -24,8 +19,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
 
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
   return app.toTree();
 };
